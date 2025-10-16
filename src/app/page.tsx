7c0390739506f83ebc02d2c9497daf6b340cbdbc
Yoeri Joëll Moens <yoerimoens@gmail.com>
2025-10-16 19:02:04 +0200

    Removed the unused Team component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Hero from '@/app/components/Hero'
 import Features from '@/app/components/Features'
-import Team from '@/app/components/Team'
 
 export default function HomePage() {
     return (
         <>
             <Hero/>
             <Features/>
-            {/*<Team/>*/}
         </>
     )
 }
